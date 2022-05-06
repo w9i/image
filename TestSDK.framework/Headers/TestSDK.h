244a5dc291bcf145add2da76a1557d78ae0bb8dc
w9i <50361448+w9i@users.noreply.github.com>
2022-05-06 17:43:57 +0800

    Add files via upload

---
+++ TestSDK.framework/Headers/TestSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestSDK.h
+//  TestSDK
+//
+//  Created by wangmac on 2022/5/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSDK.
+FOUNDATION_EXPORT double TestSDKVersionNumber;
+
+//! Project version string for TestSDK.
+FOUNDATION_EXPORT const unsigned char TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSDK/PublicHeader.h>
+
+
